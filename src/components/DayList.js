085cd3e0d92c2a7d4db6d20df80623a9fbf6274b
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-19 12:03:54 -0600

    refactoring for the conrolled lists(changed the key name)

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,15 @@ import React from "react";
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-
+  
   const listDays = props.days.map((day) => {
-    const propObj = {
-      ...day,
-      selected: day.name === props.day,
-      setDay: props.setDay
-    }
-    return <DayListItem key={day.id} {...propObj} />
+    return (<DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.value}
+      setDay={props.onChange}
+      />);
   })
 
   return (
