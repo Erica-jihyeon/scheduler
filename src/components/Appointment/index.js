bf639c6057069522132cb885646c11d128d89eaf
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-19 20:36:00 -0600

    tested displaying appointments on the storybook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "components/Appointment/styles.scss";
 import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 
 export default function Appointment(props) {
-
+  console.log(props)
   return (
-    <article className="appointment">{!props.time ? "no appointments" : `Appointment at ${props.time}`}</article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty />}
+    </article>
   );
 }
