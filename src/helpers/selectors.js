2b226a952edc0faa4f13f4ae6f27571a4a9199c0
Erica-jihyeon <jihyeon20@gmail.com>
2022-01-21 20:55:52 -0600

    added getInterviewersForDay helper function and completed the test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -25,3 +25,22 @@ export function getInterview(state, interview) {
     "interviewer": state.interviewers[interview.interviewer]
   }
 }
+
+
+export function getInterviewersForDay(state, day) {
+  //find the day(return like ["Monday"])
+  const filteredDay = state.days.filter(item => item.name === day);
+  
+  const result = [];
+  /**
+   * if day is not found then return empty array
+   * otherwise return an array of the interviewer info object
+   * */
+  if (filteredDay.length !== 0) {
+    filteredDay[0].interviewers.map(num => {
+      return result.push(state.interviewers[num]);
+    })
+  }
+
+  return result;
+}
